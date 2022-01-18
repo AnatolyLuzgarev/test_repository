97330787eeb1f3fad969fb93b085acceb95a3bf5
Tolik <IngrayTolik@yandex.ru>
2022-01-18 21:57:23 +0300

    third commit

--- test_git.py
+++ test_git.py
@@ -8,3 +8,10 @@ import math
 def sqrt_approx(x):
 	appr = 1 - x/2
 	return appr
+
+def mult_vectors(v_1, v_2):
+	result = 0
+	for i in range(len(v_1)):
+		result = result + v_1[i]*v_2[i]
+	return result
+
